f2bff7cdab8297919d9fd51ac9564643b2e9426f
Branden Mosley <branden.mosley1337@gmail.com>
2018-01-11 05:49:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+  var lowercase = "hello!"
+  var mixedCase = "I love you, Grandma."
+  
+  if (string === lowercase) {
+    return "I can't hear you!"
+  } else if (string === uppercase) {
+    return "YES INDEED!"
+  } else (string === mixedCase) {
+    return "I love you, too."
+  }
+}
